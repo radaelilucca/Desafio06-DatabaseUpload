f167bbdc7e554d13cc893eb3de3cae0f4d7e997f
Lucca Radaeli <radaelidev@gmail.com>
2020-08-24 19:00:06 -0300

    feat: delete transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,39 +2,41 @@ import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {
-  // TODO
-});
+// transactionsRouter.get('/', async (request, response) => {
+//   // TODO
+// });
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
 
-  try {
-    const createTransaction = new CreateTransactionService();
+  const createTransaction = new CreateTransactionService();
 
-    const transaction = await createTransaction.execute({
-      title,
-      value,
-      type,
-      category,
-    });
-    return response.json(transaction);
-  } catch (error) {
-    return response.json({ error: error.message });
-  }
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  const transaction = await deleteTransaction.execute({ id });
+
+  return response.json(transaction);
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+// transactionsRouter.post('/import', async (request, response) => {
+//   // TODO
+// });
 
 export default transactionsRouter;
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,32 @@
+import { getRepository } from 'typeorm';
+
+import validate from 'uuid-validate';
+
 import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+
+    const validId = validate(id);
+
+    if (!validId) {
+      throw new AppError('Invalid uuid', 401);
+    }
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('This transaction does not exists', 404);
+    }
+    await transactionsRepository.remove(transaction);
+
+    return transaction;
   }
 }
 
